c99b819a44d8844c0c20d854982b6ea5b491b063
omgbqh98 <omgbqh@gmail.com>
2023-11-08 21:08:20 +0700

    Revert "hearder"
    This reverts commit 9fce6b2841baa0139b656b72b6bb5cdd88b8dbb9.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.scss';
-import Header from './components/header/Header';
-import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className='app-container'>
-      <Header></Header>
-      <div>
-        test link
-      </div>
-      <div>
-        <button>
-          <Link to="/users">go User</Link>
-        </button>
-        <button>
-          <Link to="/admin">go admin</Link>
-        </button>
-      </div>
+      hello
+      <button className='btn btn-primary'>ok</button>
     </div>
   );
 }
