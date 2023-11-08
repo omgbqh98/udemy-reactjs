9fce6b2841baa0139b656b72b6bb5cdd88b8dbb9
omgbqh98 <omgbqh@gmail.com>
2023-11-08 21:07:44 +0700

    hearder

---
+++ src/components/admin/Admin.js
@@ -0,0 +1,9 @@
+const Admin = () => {
+    return (
+        <div>
+            Admin components
+        </div>
+    )
+}
+
+export default Admin;
---
+++ src/components/header/Header.js
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">Quang Huynh</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link className='nav-link' to="/">Home</Link>
+                        <Link className='nav-link' to="/users">User</Link>
+                        <Link className='nav-link' to="/admin">Admin</Link>
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="Setting" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">logout</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
